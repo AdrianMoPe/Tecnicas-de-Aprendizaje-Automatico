5fed98a746febcf09c692002f81ee958596c4b4d
AdrianMoPe <adrianmoraperela@gmail.com>
2017-10-30 19:10:45 +0100

    Update clusteringFeatures.py

--- Activity_2/clusteringFeatures.py
+++ Activity_2/clusteringFeatures.py
@@ -13,33 +13,33 @@ from sklearn.decomposition import PCA
 from scipy import cluster
 import sklearn.neighbors
 
-# 0. Load Data
+# Load Data
 iquito,names = loaddata.loaddata("denguepivot.csv")
 features = numpy.transpose(iquito);
 
-#1. Normalization of the data
+# Normalization of the data
 min_max_scaler = preprocessing.MinMaxScaler()
 features_norm = min_max_scaler.fit_transform(features)
 
-#1.2. Principal Component Analysis
+# Principal Component Analysis
 estimator = PCA (n_components =2)
 X_pca = estimator.fit_transform(features_norm)
 print("Variance Ratio: ", estimator.explained_variance_ratio_) 
 
-# 2. Compute the similarity matrix
+# Compute the similarity matrix
 dist = sklearn.neighbors.DistanceMetric.get_metric('euclidean')
 matsim = dist.pairwise(features_norm)
 avSim = numpy.average(matsim)
 print "%s\t%6.2f" % ('Average Distance', avSim)
 
-# 3. Building the Dendrogram	
+# Building the Dendrogram	
 cut=4
 clusters = cluster.hierarchy.linkage(matsim, method = 'complete')
 cluster.hierarchy.dendrogram(clusters, color_threshold = cut, labels = names, leaf_rotation=90)
 plt.title('Method: Complete. Cut: %d' %cut)
 plt.show()
 
-#3.1 Show the Dendrogram
+# Show the Dendrogram
 labels = cluster.hierarchy.fcluster(clusters, cut , criterion = 'distance')
 print 'Number of clusters: %d' % (len(set(labels)))
 
@@ -48,7 +48,7 @@ colors = numpy.hstack([colors] * 20)
 numbers = numpy.arange(len(X_pca))
 fig, ax = plt.subplots()
 
-#4. Show XPCA
+# Show XPCA
 for i in range(len(X_pca)):         
     plt.text(X_pca[i][0], X_pca[i][1], i+4, color=colors[labels[i]]) 
 
