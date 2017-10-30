24b5dd34f13335cd471d8d334a9041470f8747d3
AdrianMoPe <adrianmoraperela@gmail.com>
2017-10-30 19:09:42 +0100

    Update clusteringElements.py

--- Activity_2/clusteringElements.py
+++ Activity_2/clusteringElements.py
@@ -4,7 +4,6 @@
 @author: Adrian Mora Perela
 @author: Sergio Grande Murillo
 '''
-
 import loaddata
 from sklearn import preprocessing 
 from sklearn.decomposition import PCA
@@ -13,38 +12,36 @@ import sklearn.neighbors
 import numpy
 from scipy import cluster
 
-
-# 1. Load data 
+# Load data 
 data, features = loaddata.loaddata("denguepivot.csv")
 
-#1. Data normalizazion
+# Data normalizazion
 min_max_scaler = preprocessing.MinMaxScaler()
 datanorm = min_max_scaler.fit_transform(data)
 
-#2. Principal Component Analysis
+# Principal Component Analysis
 estimator = PCA (n_components = 2)
 X_pca = estimator.fit_transform(datanorm)
 print("Variance Ratio: ", estimator.explained_variance_ratio_) 
 
-#3. Hierarchical Clustering
-# 3.1. Compute the similarity matrix
+# Hierarchical Clustering
+# Compute the similarity matrix
 dist = sklearn.neighbors.DistanceMetric.get_metric('euclidean')
 matsim = dist.pairwise(X_pca)
 avSim = numpy.average(matsim)
 print "%s\t%6.2f" % ('Average Distance', avSim)
 
-# 3.2. Building the Dendrogram	
+# Building the Dendrogram	
 clusters = cluster.hierarchy.linkage(matsim, method = 'complete')
 cut = 6 
 cluster.hierarchy.dendrogram(clusters, color_threshold=cut)
 plt.title('Method: Complete. Cut: %d' %cut)
 plt.show()
-
-#4. 
+ 
 labels = cluster.hierarchy.fcluster(clusters, cut , criterion = 'distance')
 print 'Number of clusters %d' % (len(set(labels)))
 
-# 5. plot PCA
+# Plot PCA
 colors = numpy.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
 colors = numpy.hstack([colors] * 20)
 numbers = numpy.arange(len(X_pca))
@@ -59,11 +56,11 @@ fig.tight_layout()
 plt.title('Method: Complete. Cut: %d. Criterion: Distance' %cut)
 plt.show()
 
-# 6. characterization
+# Characterization
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 print('Estimated number of clusters: %d' % n_clusters_)
 
-#5.1 Show Groups
+# Show Groups
 for c in range(1,n_clusters_+1):
     print 'Group', c
     for i in range(len(datanorm[0])):
@@ -72,7 +69,7 @@ for c in range(1,n_clusters_+1):
             print "-",features[i],":", numpy.mean(column)
 
 
-#7. Show the elements which belong to each group
+# Show the elements which belong to each group
 for c in range(1,n_clusters_+1):
     grupos=[]
     for i in range(len(data)): 
